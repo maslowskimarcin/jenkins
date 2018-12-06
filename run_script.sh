5f455c25db9b70fb5ea64f4880c4fe50f575a707
maslowskimarcin <maslowski.it@gmail.com>
2018-12-06 12:48:42 +0100

    First commit

---
+++ run_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 /home/maslo/sample.py
---
+++ run_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if python3 -c "import pytest"
+then
+   echo "Nothing to install"
+else
+    pip3 install --upgrade --user pytest
+fi
+python3 -m pytest --junitxml results.xml /home/maslo/tests.py
